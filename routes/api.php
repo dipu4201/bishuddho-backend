08ce27ae598d3aca3eddeb83c567c29eb762cd00
Delower Hossain <delowerdipu420@gmail.com>
2026-09-03 15:16:59 +0600

    Add routes for categories and products

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
@@ -8,3 +10,9 @@ Route::get('/test', function () {
         'message' => 'BISHUDDHO API is connected successfully!',
     ]);
 });
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{slug}', [CategoryController::class, 'show']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{slug}', [ProductController::class, 'show']);
